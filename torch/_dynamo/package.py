69b785f0a057d3aa1afefb80afea9ee6382e8ba7
zhxchen17 <zhxchen17@fb.com>
2025-07-02 12:48:17 -0700

    [wip] torch._dynamo.save/load() for saving and loading compiled models.
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/157481
    
    
    ghstack-source-id: 293944009
    
    Differential Revision: [D77676388](https://our.internmc.facebook.com/intern/diff/D77676388/)

--- torch/_dynamo/package.py
+++ torch/_dynamo/package.py
@@ -615,6 +615,8 @@ class DiskDynamoStore(DynamoStore):
         Save a package to disk using the path_prefix + key as the file path.
         """
         full_path = os.path.join(self.path_prefix, key) if self.path_prefix else key
+        if not os.path.exists(full_path):
+            os.makedirs(full_path)
         super().save_package(package, full_path)
 
     def load_package(
