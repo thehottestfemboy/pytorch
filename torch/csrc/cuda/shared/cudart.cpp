fc0d6190ae269b80b2405efa8fea7fa8b6ae39e5
Benjamin Glass <bglass@openteams.com>
2025-08-20 20:45:01 +0000

    Update pybind11 enums to new native_enum class
    Updates all our PyBind11 wrapped enums to use the new `native_enum` class, which is PEP 435 compliant.
    
    Uses the following decision tree to determine which base enum class to give each enum:
    
    1. If the enum is documented in a PYI file, use the documented base class.  In one or two places, this necessitated updating the documented PYI base class (when integer constant values were provided, but the base class was `Enum` rather than `IntEnum`) first.
    2. If the enum is undocumented, default to using `IntEnum` for maximum backwards compatibility.
    
    Additionally this fixes one or two typing bugs, and updates source files as minimally as possible to address linter complaints triggered by touching these files.
    
    ghstack-source-id: ff71667e64de81e67304c779c7c82d5e35f0670e
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/160771

--- torch/csrc/cuda/shared/cudart.cpp
+++ torch/csrc/cuda/shared/cudart.cpp
@@ -1,3 +1,5 @@
+#include <type_traits>
+
 #include <cuda.h>
 #include <cuda_runtime.h>
 #include <torch/csrc/utils/pybind.h>
@@ -20,7 +22,7 @@ hipError_t hipReturnSuccess() {
 } // namespace
 #endif
 
-void initCudartBindings(PyObject* module) {
+extern void initCudartBindings(PyObject* module) {
   auto m = py::handle(module).cast<py::module>();
 
   auto cudart = m.def_submodule("_cudart", "libcudart.so bindings");
@@ -31,24 +33,32 @@ void initCudartBindings(PyObject* module) {
 #if !defined(USE_ROCM) && defined(CUDA_VERSION) && CUDA_VERSION < 12000
   // cudaOutputMode_t is used in cudaProfilerInitialize only. The latter is gone
   // in CUDA 12.
-  py::enum_<cudaOutputMode_t>(
+  py::native_enum<cudaOutputMode_t>(
       cudart,
       "cuda"
-      "OutputMode")
+      "OutputMode",
+      "enum.IntEnum")
       .value("KeyValuePair", cudaKeyValuePair)
-      .value("CSV", cudaCSV);
+      .value("CSV", cudaCSV)
+      .finalize();
 #endif
 
-  py::enum_<cudaError_t>(
+  py::native_enum<cudaError_t>(
       cudart,
       "cuda"
-      "Error")
-      .value("success", cudaSuccess);
+      "Error",
+      "enum.IntEnum")
+      .value("success", cudaSuccess)
+      .finalize();
 
   cudart.def(
       "cuda"
       "GetErrorString",
-      cudaGetErrorString);
+      // Since we only wrap one enum value directly, do an indirection here to
+      // accept any returned error code as an input to this function.
+      [](std::underlying_type_t<cudaError_t> error_enum) {
+        return cudaGetErrorString(static_cast<cudaError_t>(error_enum));
+      });
   cudart.def(
       "cuda"
       "ProfilerStart",
